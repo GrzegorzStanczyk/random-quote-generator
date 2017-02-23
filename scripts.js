d27a35ac241e698cde5daf0a8eb7a2a126f5ea0e
Grzegorz Stańczyk <grzegorz.stanczyk@o2.pl>
2017-02-23 21:12:03 +0100

    Performance improvements

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 (function() {
-  
-  let quotes = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=JSONPCallback";
-  
-  let quoteContainer = document.querySelector("#quote"),
+
+  const quotes = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=JSONPCallback";
+
+  const quoteContainer = document.querySelector("#quote"),
       author = document.querySelector("span"),
       cite = document.querySelector("p"),
       // tweet = document.querySelector("#tweet"),
@@ -10,11 +10,11 @@
       frag = document.createDocumentFragment();
 
 
-  var jsonp = {
+  const jsonp = {
       callbackCounter: 0,
 
       fetch: function(url, callback) {
-          
+
           var fn = 'JSONPCallback_' + this.callbackCounter++;
           window[fn] = this.evalJSONP(callback);
           url = url.replace('=JSONPCallback', '=' + fn);
@@ -44,35 +44,32 @@
           }
       }
   };
-  
 
-  let loadStuff = (data) => {
+
+  const loadStuff = (data) => {
 
       let citeContent = JSON.stringify(data[0].content)
                             .replace(/<[^>]*>|\\n|;/g,"")
                             .replace(/&#8217|&#8221|&#8220/g,"'");
       cite.textContent = citeContent;
       frag.appendChild(cite);
-    
+
       let citeAuthor = JSON.stringify(data[0].title)
                            .replace(/"/g,"");
       author.textContent = citeAuthor;
       frag.appendChild(author);
-    
+
       quoteContainer.appendChild(frag);
-    
+
       loadTwitter(citeContent, citeAuthor);
-    
+
   };
-  
-  let loadTwitter = (citeContent, citeAuthor) => {
+
+  const loadTwitter = (citeContent, citeAuthor) => {
     tweet.href = `https://twitter.com/intent/tweet?text=${citeContent} ${citeAuthor}`;
   };
 
   document.querySelector("#loadData").onclick = () => {
-      
-      quoteContainer.removeChild(cite);
-      quoteContainer.removeChild(author);
 
         jsonp.fetch(quotes, loadStuff);
   };
